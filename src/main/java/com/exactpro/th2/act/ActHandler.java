3d1731763b2f7e2ff08421e8798ac755d2ec0e6f
Oleg Smelov <oleg.smelov@exactpro.com>
2024-02-01 07:49:23 +0100

    deps updated version updated README.md updated

--- src/main/java/com/exactpro/th2/act/ActHandler.java
+++ src/main/java/com/exactpro/th2/act/ActHandler.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.exactpro.th2.act;
 
 import com.exactpro.th2.act.grpc.ActGrpc.ActImplBase;
